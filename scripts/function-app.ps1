5c8adae4ba0c3b239230aa33a74a9d47c130936a
Jim Kulba <Jim.Kulba@dfinsolutions.com>
2022-10-24 09:16:32 -0700

    flatten out commands

--- scripts/function-app.ps1
+++ scripts/function-app.ps1
@@ -1,19 +1,24 @@
 ## Create a new Azure resource group
 resourcegroup=rgheimdall001dev
-az group create --name $resourcegroup --location eastus
+az group create --name rgheimdall001dev --location eastus
 
 ## Validate deployment and parameters
 az deployment group validate \
---resource-group $resourcegroup \
+--resource-group rgheimdall001dev \
 --template-file function-app.json \
 --parameters @function-app.parameters.json
+
+az deployment group validate --resource-group rgheimdall001dev --template-file function-app.json --parameters @function-app.parameters.json
+
 
 ## Review the deployment without deploying
 az deployment group what-if \
 --name HeimdallFunctionAppDeployment \
---resource-group $resourcegroup \
+--resource-group rgheimdall001dev \
 --template-file function-app.json \
 --parameters @function-app.parameters.json
+
+az deployment group what-if --name HeimdallFunctionAppDeployment --resource-group rgheimdall001dev --template-file function-app.json --parameters @function-app.parameters.json
 
 ## Create a new deployment
 az deployment group create \
@@ -21,3 +26,5 @@ az deployment group create \
 --resource-group $resourcegroup \
 --template-file function-app.json \
 --parameters @function-app.parameters.json
+
+az deployment group create --name HeimdallFunctionAppDeployment --resource-group rgheimdall001dev --template-file function-app.json --parameters @function-app.parameters.json
